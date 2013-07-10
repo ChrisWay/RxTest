2daafb1396dd2d2d1481e7a197ac1ed26e1f2ac9
ChrisWay <chris@darksquare.net>
2013-07-10 11:22:25 +0100

    Test Projects

---
+++ RxPhoneTest/TestControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ReactiveUI;
+
+namespace RxPhoneTest
+{
+	public partial class TestControl : UserControl
+	{
+		public TestControl()
+		{
+			InitializeComponent();
+			if (DesignModeDetector.IsInDesignMode())
+				AmIDesign.Text = "Yes!";
+			else
+				AmIDesign.Text = "No!";
+		}
+	}
+}
---
+++ RxTestWinRT/TestControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RxTestWinRT
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class TestControl : Page
+	{
+		public TestControl()
+		{
+			this.InitializeComponent();
+
+			if (DesignModeDetector.IsInDesignMode())
+			{
+				AmIDesign.Text = "Yes!";
+			}
+			else
+			{
+				AmIDesign.Text = "No!";
+			}
+		}
+
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.  The Parameter
+		/// property is typically used to configure the page.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+		}
+	}
+}
